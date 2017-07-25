3c964483865c22fc66927010d491c60bb8da6a41
linhao <linhao09@meituan.com>
2017-07-25 14:23:13 +0800

    repeatable combine

--- src/main/java/me/hao0/algo/combine/BinaryCombine.java
+++ src/main/java/me/hao0/algo/combine/BinaryCombine.java
@@ -1,6 +1,6 @@
 package me.hao0.algo.combine;
 
-public class SimpleCombine {
+public class BinaryCombine {
 
     public static void main(String[] args) {
 
---
+++ src/main/java/me/hao0/algo/combine/RepeatableCombine.java
@@ -0,0 +1,35 @@
+package me.hao0.algo.combine;
+
+public class RepeatableCombine {
+
+    public void runPermutation(int[] a) {
+
+      if (null == a || a.length == 0) {
+        return;
+      }
+        int[] b = new int[a.length];// 辅助空间，保存待输出排列数
+        getAllPermutation(a, b, 0);
+    }
+
+    public void getAllPermutation(int[] a, int[] b, int index) {
+        // 当index和数组的长度一致的时候打印
+        if (index == a.length) {
+            for (int i = 0; i < index; i++) {
+                System.out.print(b[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            b[index] = a[i];
+            getAllPermutation(a, b, index + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        RepeatableCombine robot = new RepeatableCombine();
+        int[] a = {1, 2, 3};
+        robot.runPermutation(a);
+    }
+}
